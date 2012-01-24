a2e8a01f06f81a681dbc2cfe305f2a8bcfee1868
Troy Will <troydwill@gmail.com>
2012-01-24 08:14:59 -0800

    02-initscripts-manual-intervention.sh

--- 02-initscripts-manual-intervention.sh
+++ 02-initscripts-manual-intervention.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # See https://www.archlinux.org/news/initscripts-update-manual-intervention-required/
 mv --interactive /etc/profile.d/locale.sh /tmp
+pacman --sync initscripts
--- 03-filesystem-manual-intervention.sh
+++ 03-filesystem-manual-intervention.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+set -o verbose
 # See http://www.archlinux.org/news/filesystem-upgrade-manual-intervention-required/
 ls -l /etc/mtab
 pacman --sync filesystem --force
-
