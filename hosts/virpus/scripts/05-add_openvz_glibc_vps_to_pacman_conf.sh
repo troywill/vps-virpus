88f29c8e496e543863828f928efd5fe60adf8ff0
Troy Will <troydwill@gmail.com>
2012-04-21 09:45:41 -0700

    new file:   60-glibc-unbork.sh

---
+++ hosts/virpus/scripts/05-add_openvz_glibc_vps_to_pacman_conf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+# 05-add-openvz_glibc_vps_to_pacman_conf.sh: A simple shell script to upgrade pacman on a new Virups VPS.
+# troydwill@gmail.com April 19, 2012. Run as root
+# Primary reference: https://wiki.archlinux.org/index.php/Virtual_Private_Server
+
+mkdir --verbose --parent BACKUPS
+cp --verbose --backup=numbered /etc/pacman.conf "BACKUPS/"
+cp --verbose --interactive etc/2.pacman.conf.glib-vps.1 /etc/pacman.conf
